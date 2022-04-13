29ab49e18dfbe7824240a86fb41f8791a6214633
vicuartas230 <vicuartas230@gmail.com>
2022-04-13 18:20:46 -0500

    Upload another tasks

---
+++ coche.js
@@ -0,0 +1,7 @@
+module.exports = class Coche {
+    constructor (peso, potencia, marca) {
+        this.peso = peso;
+        this.potencia = potencia;
+        this.marca = marca;
+    }
+}
---
+++ random.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < 10; i++) {
+    x = Math.random() * 21;
+    console.log(Math.floor(x));
+}
